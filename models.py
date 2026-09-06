05f8472e7f08e2801f9c8b1606b8f4feef07d95a
retete185-a11y <retete185@gmail.com>
2026-09-06 09:54:40 +0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,39 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+
+from database import Base
+
+
+class User(Base):
+
+    __tablename__ = "users"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
+
+    username = Column(
+        String(24),
+        unique=True,
+        nullable=False,
+        index=True
+    )
+
+    email = Column(
+        String(255),
+        unique=True,
+        nullable=False,
+        index=True
+    )
+
+    password_hash = Column(
+        String(255),
+        nullable=False
+    )
+
+    created_at = Column(
+        DateTime,
+        default=datetime.utcnow
+    )
